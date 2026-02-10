e7f1d990ea2a0964e47853df1a7e228f9e71d3e7
MiUsuario <abelardolg.it@gmail.com>
2026-02-10 11:11:29 +0100

    Ready to accept this refactoring process

--- src/main/java/com/spotter_proyect/spotter/core/shared/entities/TrainerEntity.java
+++ src/main/java/com/spotter_proyect/spotter/core/shared/entities/TrainerEntity.java
@@ -1,9 +1,11 @@
 package com.spotter_proyect.spotter.core.shared.entities;
 
+import com.spotter_proyect.spotter.core.shared.enums.Specialty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "trainers")
@@ -15,13 +17,17 @@ public class TrainerEntity extends UserEntity {
     @Column(columnDefinition = "TEXT")
     private String biography;
 
-    private String specialty; // Yoga, Crossfit...
+    @Enumerated(EnumType.STRING)
+    @Column(name="specialty")
+    private Specialty specialty; // Yoga, Crossfit...
 
+    @Column(name="phoneNumber")
     private String phoneNumber; // El dato "oculto" hasta el match
 
-    private Boolean isVerified = false;
+    @Column(name="isVerified")
+    private boolean isVerified = false;
 
-    // Relación: Un entrenador tiene muchos videos
+    // Relación 1:N: 1 entrenador tiene N vídeos; 1 vídeo solo pertenece a 1 entrenador
     @OneToMany(mappedBy = "trainer", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<VideoEntity> videos;
+    private Set<VideoEntity> videos;
 }
