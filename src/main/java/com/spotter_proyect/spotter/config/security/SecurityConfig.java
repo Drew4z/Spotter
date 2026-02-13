c7e3c202a74ded2b5ac96b24c1a8a27d4f1cf60a
Drew4z <andrewwac27@gmail.com>
2026-02-13 11:46:17 +0100

    add security paths && auth provider

--- src/main/java/com/spotter_proyect/spotter/config/security/SecurityConfig.java
+++ src/main/java/com/spotter_proyect/spotter/config/security/SecurityConfig.java
@@ -1,22 +1,40 @@
 package com.spotter_proyect.spotter.config.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter; // Tu portero
+    private final AuthenticationProvider authenticationProvider; // Tu proveedor
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Necesario para que funcionen los POST
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // <--- ¡AQUÍ ESTÁ LA CLAVE! (Permitir todo)
-                );
+                        // 1. Rutas PÚBLICAS (Login y Register) -> Permitir a todos
+                        .requestMatchers("/auth/**").permitAll()
+
+                        // 2. RESTO de rutas -> Requieren TOKEN
+                        .anyRequest().authenticated()
+                )
+                // 3. No guardar sesión (Stateless) porque usamos Tokens
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // 4. Añadir el proveedor y el filtro
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
